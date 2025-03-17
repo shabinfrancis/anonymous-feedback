07ff41031a4943ee43930ce333e54042bbb307e3
Shabin Francis <shabinfrancis25@gmail.com>
2025-03-18 00:19:36 +0530

    Designed get-messages API to get messages of currently logged-in user

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,51 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+    const user: User = session?.user as User;  //Assertion
+    if (!session || !session.user) {
+        return Response.json(
+            {
+                success: false,
+                message: "Not Authenticated"
+            }, { status: 401 }
+        )
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);   //The type of _id is converted from string to ObjectId using mongoose. Now it won't be an issue while writing aggregation pipeline
+    try {
+        const user = await UserModel.aggregate([
+            { $match: { id: userId } },
+            { $unwind: '$messages' },
+            { $group: { _id: '$_id', messages: { $push: '$messages' } } }
+        ])
+        if(!user || user.length === 0) {
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                }, { status: 401 }
+            )
+        } 
+        return Response.json(
+            {
+                success: true,
+                messages: user[0].messages
+            }, { status: 200 }
+        )
+    } catch (error) {
+        console.log("An unexpected error occured: ", error)
+        return Response.json(
+            {
+                success: false,
+                message: "Internal server error"
+            }, { status: 500 }
+        )
+    }
+}
