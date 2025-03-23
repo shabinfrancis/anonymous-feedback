5601f0af23a7f043db7bf77cf3ca93cf15e625d0
Shabin Francis <shabinfrancis25@gmail.com>
2025-03-23 23:28:00 +0530

    Basic setup of User dashboard

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import './globals.css'
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/sonner"
 import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
@@ -27,15 +24,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <AuthProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
         {children}
       </body>
-      <Toaster />
-      </AuthProvider>
         
     </html>
   );
--- src/app/u/layout.tsx
+++ src/app/u/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import './globals.css'
 import AuthProvider from "@/context/AuthProvider";
 import { Toaster } from "@/components/ui/sonner"
-import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +29,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
         {children}
       </body>
       <Toaster />
