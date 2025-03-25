e57484a60cd8da1df5c336a74e5b2c9b58a318e4
Shabin Francis <shabinfrancis25@gmail.com>
2025-03-26 01:02:18 +0530

    Added fetchAcceptingMessage function in Dashboard

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,45 @@
-import React from 'react'
+'use client'
+import { Message } from '@/model/User'
+import { acceptMessageSchema } from '@/schemas/acceptMessageSchema'
+import { ApiResponse } from '@/types/ApiResponse'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios, { AxiosError } from 'axios'
+import { useSession } from 'next-auth/react'
+import React, { useCallback, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { toast, Toaster } from 'sonner'
 
 const page = () => {
+  const [messages, setMessages] = useState<Message[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false)
+
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId))
+  }
+
+  const {data: session} = useSession()
+
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema)
+  })
+
+  const {register, watch, setValue} = form;
+  const acceptMessages = watch('acceptMessages')
+
+  const fetchAcceptMessage = useCallback(async () => {
+      setIsSwitchLoading(true)
+      try {
+        const response = await axios.get('/api/accept-messages')
+        setValue('acceptMessages', response.data.isAcceptingMessage);
+      } catch (error) {
+        const axiosError = error as AxiosError<ApiResponse>;
+        toast.error(`${axiosError.response?.data.message || 'Failed to fetch message settings'}`)
+      } finally{
+        setIsSwitchLoading(false)
+      }
+  }, [setValue])
+
   return (
     <div>Dashboard</div>
   )
